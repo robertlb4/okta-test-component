b9e11cd8445917d945adbde10b19e18caf84e8b3
Gino <galentro451@gmail.com>
2020-09-08 12:25:44 -0700

    ng add support

---
+++ src/ng-add/index.ts
@@ -0,0 +1,10 @@
+import { chain, Rule, schematic, SchematicContext, Tree, } from '@angular-devkit/schematics';
+
+export default function (options: any): Rule {
+    return (host: Tree, context: SchematicContext) => {
+        return chain([
+            schematic('okta-test-component', options)
+        ])(host, context);
+    };
+}
+
